d7c81efc6f390a3f79678206ec1236d9c16ef76b
Muhammad Awais <muhammadawais1194@gmail.com>
2026-09-12 14:21:22 +0500

    refactor: use environment-specific Terraform root configuration

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-aws-platform-foundation-state-132218943329"
-    key          = "dev/terraform.tfstate"
-    region       = "ap-south-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
